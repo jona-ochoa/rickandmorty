7144b9ed626541ab5944f978bd00aaa5805bc305
jochoa <jonatan.c.ochoa@gmail.com>
2023-06-19 18:47:17 -0300

    Update deploy

--- Server/src/index.js
+++ Server/src/index.js
@@ -3,7 +3,7 @@ const { conn } = require("./DB_connection");
 
 const PORT = 3001;
 
-conn.sync({ force: true }); // conexion sequelize y postgres
+conn.sync({ force: false }); // conexion sequelize y postgres
 
 server.listen(PORT, () => {
   console.log(`Server raised in port: ${PORT}`);
