4033eff4b224b422cc9068c35487c3e5ac4f562b
jochoa <jonatan.c.ochoa@gmail.com>
2023-11-09 10:47:05 -0300

    login and register in app done

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Routes, Route, useLocation, Outlet, useNavigate } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Cards from "./components/Cards/Cards";
 import Nav from "./components/Navbar/Nav";
@@ -7,6 +7,7 @@ import About from "./components/About/About";
 import Detail from "./components/Detail/Detail";
 import GlobalStyles from "./GlobalStyles";
 import Form from "./components/Form/Form";
+import Register from "./components/RegisterForm";
 import Favorites from "./components/Favorites/Favorites";
 
 const App = () => {
@@ -34,7 +35,7 @@ const App = () => {
       const URL = "https://rickandmorty-m2y1.onrender.com/rickandmorty/register";
       await axios.post(URL, { email, password });
       // Si el registro es exitoso, también puedes iniciar sesión automáticamente
-      login(userData);
+      register(userData);
     } catch (error) {
       console.log(error.message);
     }
@@ -56,18 +57,9 @@ const App = () => {
       <GlobalStyles />
       {location.pathname !== "/" && <Nav onSearch={onSearch} logout={logout} />}
       <Routes>
-        <Route path="/" element={<Form login={login} register={register} />} />
-        <Route
-          path="/home"
-          element={
-            <Outlet>
-              <PrivateRoute
-                path="/"
-                element={<Cards characters={characters} onClose={onClose} />}
-              />
-            </Outlet>
-          }
-        />
+        <Route path="/" element={<Form login={login} />} />
+        <Route path="/register" element={<Register register={register} />} />
+        <Route path="/home" element={<Cards characters={characters} onClose={onClose} />} />
         <Route path="/about" element={<About />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="/favorites" element={<Favorites />} />
@@ -77,8 +69,3 @@ const App = () => {
 };
 
 export default App;
-
-const PrivateRoute = ({ element }) => {
-  const isAuthenticated = /* lógica para verificar la autenticación, por ejemplo, desde el contexto de autenticación */;
-  return isAuthenticated ? element : <Navigate to="/" replace />;
-};
