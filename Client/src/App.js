61af8c4fc540f1ff849769b8883a6a384b8ac3a5
jochoa <jonatan.c.ochoa@gmail.com>
2023-11-09 11:04:12 -0300

    add function onClose character and onSearch

--- Client/src/App.js
+++ Client/src/App.js
@@ -16,11 +16,38 @@ const App = () => {
   const [access, setAccess] = useState(false);
   const [characters, setCharacters] = useState([]);
 
+  async function onSearch(id) {
+    try {
+      const { data } = await axios(
+        `https://rickandmorty-m2y1.onrender.com/rickandmorty/character/${id}`
+      );
+
+      if (data.name) {
+        const characterExists = characters.find(
+          (character) => character.id === data.id
+        );
+        if (!characterExists) {
+          setCharacters((oldChars) => [...oldChars, data]);
+        } else {
+          alert("¡Ya existe un personaje con este ID!");
+        }
+      }
+    } catch (error) {
+      alert("¡No hay personajes con este ID!");
+    }
+  }
+
+  const onClose = (id) => {
+    setCharacters(characters.filter((char) => char.id !== id));
+  };
+
   async function login(userData) {
     try {
       const { email, password } = userData;
       const URL = "https://rickandmorty-m2y1.onrender.com/rickandmorty/login";
-      const { data } = await axios(URL + `?email=${email}&password=${password}`);
+      const { data } = await axios(
+        URL + `?email=${email}&password=${password}`
+      );
       const { access } = data;
       access && navigate("/home");
     } catch (error) {
@@ -31,7 +58,8 @@ const App = () => {
   async function register(userData) {
     try {
       const { email, password } = userData;
-      const URL = "https://rickandmorty-m2y1.onrender.com/rickandmorty/register";
+      const URL =
+        "https://rickandmorty-m2y1.onrender.com/rickandmorty/register";
       await axios.post(URL, { email, password });
       // Si el registro es exitoso, también puedes iniciar sesión automáticamente
       register(userData);
@@ -57,8 +85,14 @@ const App = () => {
       {location.pathname !== "/" && <Nav onSearch={onSearch} logout={logout} />}
       <Routes>
         <Route path="/" element={<Form login={login} />} />
-        <Route path="/register" element={<RegisterForm register={register} />} />
-        <Route path="/home" element={<Cards characters={characters} onClose={onClose} />} />
+        <Route
+          path="/register"
+          element={<RegisterForm register={register} />}
+        />
+        <Route
+          path="/home"
+          element={<Cards characters={characters} onClose={onClose} />}
+        />
         <Route path="/about" element={<About />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="/favorites" element={<Favorites />} />
